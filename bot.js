1f4f12a3175191cc7e87a9cdb058e2b4006bfbb2
Mikhail <miki21405242@gmail.com>
2021-06-03 02:41:01 +0300

    init commit

--- bot.js
+++ bot.js
@@ -1,13 +1,12 @@
 'use srtict'
+
 const Text = require('./StartText');
-const table = new Text;
+const messg = new Text;
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = '1675981367:AAFloUvIk-ikV-oFben8V06QDbrPylyGbwg';
 const bot = new TelegramBot(token, {polling: true});
-const temp = table.text();
-console.log(temp);
-
+const temp = messg.text();
 
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, temp['text']);
@@ -16,11 +15,11 @@ bot.onText(/\/start/, (msg) => {
 bot.on('message', (msg) => {
     const hi = "привет";
     if (msg.text.toString().toLowerCase().indexOf(hi) === 0){
-        bot.sendMessage(msg.chat.id,"Шо ты");
+        bot.sendMessage(msg.chat.id,"Привет");
     }
     const stop = "stop";
     if (msg.text.toString().toLowerCase().includes(stop)){
-        bot.sendMessage(msg.chat.id, "давай");
+        bot.sendMessage(msg.chat.id, "see ya");
     }
 });
 
