b4d6be8f123401203eb806aee0df5113853b5bff
Mikhail <miki21405242@gmail.com>
2021-06-04 22:09:22 +0300

    improvements

--- bot.js
+++ bot.js
@@ -1,48 +1,62 @@
-'use srtict'
+'use srtict';
 
 const TelegramBot = require('node-telegram-bot-api');
 const StartText = require('./StartText');
+const tok = require('./token');
 const kb = require('./keyboard_buttons');
 const keyboard = require('./keyboard');
-const keyboard_buttons = require('./keyboard_buttons');
 
-const token = '1675981367:AAFloUvIk-ikV-oFben8V06QDbrPylyGbwg';
-const bot = new TelegramBot(token, {polling: true});
+const token = tok.token;
+const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, StartText.stext,{
-        reply_markup: {
-            keyboard: keyboard.home,
-            resize_keyboard: true
-        }
-    });
+bot.onText(/\/start/, msg => {
+  bot.sendMessage(msg.chat.id, StartText.stext, {
+    reply_markup: {
+      keyboard: keyboard.home,
+      resize_keyboard: true
+    }
+  });
 });
 
-bot.on('message', (msg) => {
-    switch(msg.text){
-        case kb.home.teeth:
-            bot.sendMessage(msg.chat.id,StartText.zdtext,{
-                reply_markup: {
-                    keyboard: keyboard.quest2
-                }
-            });
-            break;
-        case kb.home.desna:
-            bot.sendMessage(msg.chat.id,StartText.zdtext,{
-                reply_markup: {
-                    keyboard: keyboard.quest3,
-                    resize_keyboard: true
-                }
-            });
-            break;
-        case kb.back:
-            bot.sendMessage(msg.chat.id,StartText.stext,{
-                reply_markup : {
-                    keyboard: keyboard.home,
-                    resize_keyboard: true
-                }
-            });
-        case kb.answer.optzf:
-            bot.sendMessage(msg.chat.id,StartText.ans);
-    }
+bot.on('message', msg => {
+  switch (msg.text) {
+  case kb.home.teeth:
+    bot.sendMessage(msg.chat.id, StartText.zdtext, {
+      reply_markup: {
+        keyboard: keyboard.quest2
+      }
+    });
+    break;
+  case kb.home.desna:
+    bot.sendMessage(msg.chat.id, StartText.zdtext, {
+      reply_markup: {
+        keyboard: keyboard.quest3,
+        resize_keyboard: true
+      }
+    });
+    break;
+  case kb.back:
+    bot.sendMessage(msg.chat.id, StartText.stext, {
+      reply_markup: {
+        keyboard: keyboard.home,
+        resize_keyboard: true
+      }
+    });
+    break;
+  case kb.answer.optzf:
+    bot.sendMessage(msg.chat.id, StartText.ans);
+    break;
+  case kb.answer.optzs:
+    bot.sendMessage(msg.chat.id, StartText.hot);
+    break;
+  case kb.answer.optzt:
+    bot.sendMessage(msg.chat.id, StartText.openm);
+    break;
+  case kb.answer.optdf:
+    bot.sendMessage(msg.chat.id, StartText.blood);
+    break;
+  case kb.answer.nothing:
+    bot.sendMessage(msg.chat.id, StartText.negansw);
+    break;
+  }
 });
--- keyboard.js
+++ keyboard.js
@@ -1,22 +1,22 @@
-'use strict'
+'use strict';
 
-const kb = require('./keyboard_buttons')
+const kb = require('./keyboard_buttons');
 
 module.exports = {
-    home: [
-        [kb.home.teeth],[kb.home.desna]
-    ],
-    quest1:[
-        [kb.quest.yes,kb.quest.no],
-        [kb.back]
-    ],
-    quest2:[
-        [kb.answer.optzf,kb.answer.optzs],
-        [kb.answer.optzt,kb.answer.nothing],
-        [kb.back]
-    ],
-    quest3:[
-        [kb.answer.optdf],
-        [kb.back]
-    ]
+  home: [
+    [kb.home.teeth], [kb.home.desna]
+  ],
+  quest1: [
+    [kb.quest.yes, kb.quest.no],
+    [kb.back]
+  ],
+  quest2: [
+    [kb.answer.optzf, kb.answer.optzs],
+    [kb.answer.optzt, kb.answer.nothing],
+    [kb.back]
+  ],
+  quest3: [
+    [kb.answer.optdf, kb.answer.nothing],
+    [kb.back]
+  ]
 };
